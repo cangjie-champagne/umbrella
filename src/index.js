d2a844a50c994d7e3cfbf4036ce23212660dee05
João Pedro <joaopjt@icloud.com>
2023-09-15 12:42:14 -0300

    added e() method values

--- src/index.js
+++ src/index.js
@@ -260,7 +260,7 @@ class Umbrella {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['heart'],
       web: []
     };
     
@@ -276,19 +276,19 @@ class Umbrella {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['day'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
       }
     }
